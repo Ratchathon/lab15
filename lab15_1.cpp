b86ae9333232b57f15d83c7e33ab7f38636a764f
Ratchathon <Ratchathon_S@cmu.ac.th>
2025-01-31 12:41:39 +0700

    lab15

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,8 +1,15 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
+    int a = 5;
+    char b = 'A';
+	char &c = b;
+    int *x = &a;
+    char *y = &b;
+    int *z = x;
+    cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<(void*)y<<" "<<z<<endl;
+	cout<<&a<<" "<<&b<<" "<<&c<<" "<<&x<<" "<<&y<<" "<<&z<<endl;
 	
 	
 	return 0;
